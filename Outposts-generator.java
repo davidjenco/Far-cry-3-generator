9aa1d829f3c3f6acf641fffd671dda643d4ab616
davidjenco <daavid.jenco@gmail.comgit config --global user.name davidjencofatalgit config --global user.email daavid.jenco@gmail.com>
2018-02-19 23:11:30 +0100

    First commit. Added files.

---
+++ Outposts-generator.java
@@ -0,0 +1,243 @@
+import java.util.Random;
+import static java.lang.System.out;
+
+public class generator {
+  public static void main(String[] args) {
+    int Brand = new Random() .nextInt(11) + 1;
+    out.print("Vybavení č.1: ");
+
+
+    if (Brand == 1) {
+      int Munice = new Random() .nextInt(16) + 1;
+      out.println("Munice: " + Munice);
+    }
+
+    if (Brand == 2) {
+      int Pistole = new Random() .nextInt(4) + 1;
+      out.println("Pistole: " + Pistole);
+    }
+
+    if (Brand == 3) {
+      int Samopaly = new Random() .nextInt(5) + 1;
+      out.println("Samopaly: " + Samopaly);
+    }
+
+    if (Brand == 4) {
+      int Brokovnice = new Random() .nextInt(3) + 1;
+      out.println("Brokovnice: " + Brokovnice);
+    }
+
+    if (Brand == 5) {
+      int Assualtrifles = new Random() .nextInt(6) + 1;
+      out.println("Útočné pušky: " + Assualtrifles);
+    }
+
+    if (Brand == 6) {
+      int Snipers = new Random() .nextInt(3) + 1;
+      out.println("Odstřelovací pušky: " + Snipers);
+    }
+
+    if (Brand == 7) {
+      int Kulomety = new Random() .nextInt(3) + 1;
+      out.println("Kulomety: " + Kulomety);
+    }
+
+    if (Brand == 8) {
+      int Boom = new Random() .nextInt(2) + 1;
+      out.println("Odpalovací pušky: " + Boom);
+    }
+
+    if (Brand == 9) {
+      int Speciality = new Random() .nextInt(4) + 1;
+      out.println("Speciality: " + Speciality);
+    }
+
+    if (Brand == 10) {
+      int Gear = new Random() .nextInt(2) + 1;
+      out.println("Spotřební zboží: " + Gear);
+    }
+
+    if (Brand == 11) {
+      int Specials = new Random() .nextInt(3) + 1;
+      out.println("Speciální zbraně: " + Specials);
+    }
+
+
+    Brand = new Random() .nextInt(11) + 1;
+    out.print("Vybavení č.2: ");
+
+
+    if (Brand == 1) {
+      int Munice = new Random() .nextInt(16) + 1;
+      out.println("Munice: " + Munice);
+    }
+
+    if (Brand == 2) {
+      int Pistole = new Random() .nextInt(4) + 1;
+      out.println("Pistole: " + Pistole);
+    }
+
+    if (Brand == 3) {
+      int Samopaly = new Random() .nextInt(5) + 1;
+      out.println("Samopaly: " + Samopaly);
+    }
+
+    if (Brand == 4) {
+      int Brokovnice = new Random() .nextInt(3) + 1;
+      out.println("Brokovnice: " + Brokovnice);
+    }
+
+    if (Brand == 5) {
+      int Assualtrifles = new Random() .nextInt(6) + 1;
+      out.println("Útočné pušky: " + Assualtrifles);
+    }
+
+    if (Brand == 6) {
+      int Snipers = new Random() .nextInt(3) + 1;
+      out.println("Odstřelovací pušky: " + Snipers);
+    }
+
+    if (Brand == 7) {
+      int Kulomety = new Random() .nextInt(3) + 1;
+      out.println("Kulomety: " + Kulomety);
+    }
+
+    if (Brand == 8) {
+      int Boom = new Random() .nextInt(2) + 1;
+      out.println("Odpalovací pušky: " + Boom);
+    }
+
+    if (Brand == 9) {
+      int Speciality = new Random() .nextInt(4) + 1;
+      out.println("Speciality: " + Speciality);
+    }
+
+    if (Brand == 10) {
+      int Gear = new Random() .nextInt(2) + 1;
+      out.println("Spotřební zboží: " + Gear);
+    }
+
+    if (Brand == 11) {
+      int Specials = new Random() .nextInt(3) + 1;
+      out.println("Speciální zbraně: " + Specials);
+    }
+
+    Brand = new Random() .nextInt(11) + 1;
+    out.print("Vybavení č.3: ");
+
+
+    if (Brand == 1) {
+      int Munice = new Random() .nextInt(16) + 1;
+      out.println("Munice: " + Munice);
+    }
+
+    if (Brand == 2) {
+      int Pistole = new Random() .nextInt(4) + 1;
+      out.println("Pistole: " + Pistole);
+    }
+
+    if (Brand == 3) {
+      int Samopaly = new Random() .nextInt(5) + 1;
+      out.println("Samopaly: " + Samopaly);
+    }
+
+    if (Brand == 4) {
+      int Brokovnice = new Random() .nextInt(3) + 1;
+      out.println("Brokovnice: " + Brokovnice);
+    }
+
+    if (Brand == 5) {
+      int Assualtrifles = new Random() .nextInt(6) + 1;
+      out.println("Útočné pušky: " + Assualtrifles);
+    }
+
+    if (Brand == 6) {
+      int Snipers = new Random() .nextInt(3) + 1;
+      out.println("Odstřelovací pušky: " + Snipers);
+    }
+
+    if (Brand == 7) {
+      int Kulomety = new Random() .nextInt(3) + 1;
+      out.println("Kulomety: " + Kulomety);
+    }
+
+    if (Brand == 8) {
+      int Boom = new Random() .nextInt(2) + 1;
+      out.println("Odpalovací pušky: " + Boom);
+    }
+
+    if (Brand == 9) {
+      int Speciality = new Random() .nextInt(4) + 1;
+      out.println("Speciality: " + Speciality);
+    }
+
+    if (Brand == 10) {
+      int Gear = new Random() .nextInt(2) + 1;
+      out.println("Spotřební zboží: " + Gear);
+    }
+
+    if (Brand == 11) {
+      int Specials = new Random() .nextInt(3) + 1;
+      out.println("Speciální zbraně: " + Specials);
+    }
+
+    Brand = new Random() .nextInt(11) + 1;
+    out.print("Vybavení č.4: ");
+
+
+    if (Brand == 1) {
+      int Munice = new Random() .nextInt(16) + 1;
+      out.println("Munice: " + Munice);
+    }
+
+    if (Brand == 2) {
+      int Pistole = new Random() .nextInt(4) + 1;
+      out.println("Pistole: " + Pistole);
+    }
+
+    if (Brand == 3) {
+      int Samopaly = new Random() .nextInt(5) + 1;
+      out.println("Samopaly: " + Samopaly);
+    }
+
+    if (Brand == 4) {
+      int Brokovnice = new Random() .nextInt(3) + 1;
+      out.println("Brokovnice: " + Brokovnice);
+    }
+
+    if (Brand == 5) {
+      int Assualtrifles = new Random() .nextInt(6) + 1;
+      out.println("Útočné pušky: " + Assualtrifles);
+    }
+
+    if (Brand == 6) {
+      int Snipers = new Random() .nextInt(3) + 1;
+      out.println("Odstřelovací pušky: " + Snipers);
+    }
+
+    if (Brand == 7) {
+      int Kulomety = new Random() .nextInt(3) + 1;
+      out.println("Kulomety: " + Kulomety);
+    }
+
+    if (Brand == 8) {
+      int Boom = new Random() .nextInt(2) + 1;
+      out.println("Odpalovací pušky: " + Boom);
+    }
+
+    if (Brand == 9) {
+      int Speciality = new Random() .nextInt(4) + 1;
+      out.println("Speciality: " + Speciality);
+    }
+
+    if (Brand == 10) {
+      int Gear = new Random() .nextInt(2) + 1;
+      out.println("Spotřební zboží: " + Gear);
+    }
+
+    if (Brand == 11) {
+      int Specials = new Random() .nextInt(3) + 1;
+      out.println("Speciální zbraně: " + Specials);
+    }
+  }
+}
